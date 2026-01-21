88f5b2fde411ffae38638b566b546d34bf648d1f
Gabriel Bauer <102755557+bauerzinn@users.noreply.github.com>
2026-01-21 20:48:48 -0300

    primeiro commit

---
+++ src/fundamentos/Main.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class Main  {
+    public void main(String[] args) {
+        System.out.println("primeiro programa!!!");
+    }
+}
+//
---
+++ src/fundamentos/Temperatura.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Temperatura {
+    public static void main(String[] args) {
+
+        final double fator = 5.0 / 9;
+        double ajuste = 32;
+
+        double fahrenheit = 86;
+        double celsius = (fahrenheit - ajuste) * fator;
+
+        System.out.println("celcius = " + celsius);
+
+    }
+}
