42db39e017d6dba316d142944806767a60391378
Gabriel Bauer <102755557+bauerzinn@users.noreply.github.com>
2026-01-26 20:29:28 -0300

    class on type inference - using var, etc

---
+++ src/fundamentos/inferencia.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class inferencia {
+    static void main() {
+        double a = 4.5;
+        System.out.println(a);
+
+        var b = 4.5 ;  // "var" identify value
+        System.out.println(b);
+
+        var c = "Texto";
+        System.out.println(c);
+
+
+        double d;  // declared the variable
+        d = 123.45; // declared the value variable
+        System.out.println(d); // print
+
+        // <-- u don't can use this with "var"
+
+    }
+}
